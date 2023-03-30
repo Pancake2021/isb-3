7e23c5e14c30283c80eaa8ba85d1128be3dcba19
Pancake2021 <92122200+Pancake2021@users.noreply.github.com>
2023-03-31 01:23:08 +0400

    otl

--- workarea.py
+++ workarea.py
@@ -33,6 +33,8 @@
 import os
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
 
 
 
