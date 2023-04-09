7ebbd4ce97f3d197c4511779551bb02e9e177975
Pancake2021 <92122200+Pancake2021@users.noreply.github.com>
2023-04-09 13:24:46 +0400

    deserialization of an encrypted file and working with text

--- Task3.py
+++ Task3.py
@@ -1,8 +1,9 @@
-import os
+
 from cryptography.hazmat.primitives.asymmetric import padding
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.serialization import load_pem_private_key
-from Task1 import
+import yaml
+#from Task1 import
 
 
 
@@ -32,3 +33,23 @@ def decrypting_data(encrypted_file_path: str, secret_key_path: str, symmetric_ke
 
     # дешифрование симметричного ключа асимметричным алгоритмом
     dsymmetric_key = private_key.decrypt(encrypted_symmetric_key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None))
+      
+    # десериализация шифрованного файла
+    encrypted_file = encrypted_file_path + '\\file.yaml'
+    with open(encrypted_file) as _file:
+        content_encrypted = yaml.safe_load(_file)
+
+    text_enc = content_encrypted["text"]
+    iv_enc = content_encrypted["iv"]
+
+    # дешифрование и депаддинг текста симметричным алгоритмом
+    cipher = Cipher(algorithms.IDEA(dsymmetric_key), modes.CBC(iv_enc))
+    decryptor = cipher.decryptor()
+    dc_text = decryptor.update(text_enc) + decryptor.finalize()
+
+    unpadder = padding2.ANSIX923(64).unpadder()
+    unpadded_dc_text = unpadder.update(dc_text) + unpadder.finalize()
+
+    decrypted_file = decrypted_file_path + '\\file.txt'
+    with open(decrypted_file, 'w') as _file:
+        _file.write(str(unpadded_dc_text))
